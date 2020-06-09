2479c6c6f62d9f83613f5b6a80baace95d55698e
Shaun Gedye <sgedye88@yahoo.com.au>
2020-06-09 12:59:45 +0800

    Updates

---
+++ precache-manifest.c849f2393588f7554f9f92e0cbca911e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f37261c9d57946b4ece065f97a5cfdf8",
+    "url": "/sgedye/meme-builder/index.html"
+  },
+  {
+    "revision": "2e0e24c8fc29472663fa",
+    "url": "/sgedye/meme-builder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dca8869e8275f971f8cf",
+    "url": "/sgedye/meme-builder/static/js/2.3e18d1c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sgedye/meme-builder/static/js/2.3e18d1c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0e24c8fc29472663fa",
+    "url": "/sgedye/meme-builder/static/js/main.f689db8f.chunk.js"
+  },
+  {
+    "revision": "6bcd2043b4731168ef19",
+    "url": "/sgedye/meme-builder/static/js/runtime-main.a69a20ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sgedye/meme-builder/static/media/logo.5d5d9eef.svg"
+  }
+]);
